58cf8b5f8dc0c7e3dc045f3e13b6fb26c0dbb347
Adnan Bhaldar <adnanbhaldar115@gmail.com>
2026-03-06 23:00:17 +0530

    rename useRepoStats to useRepoStats.ts

--- docs/.vitepress/theme/components/StatsBanner.vue
+++ docs/.vitepress/theme/components/StatsBanner.vue
@@ -40,7 +40,7 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from "vue";
 import CountUp from "vue-countup-v3";
-import { useRepoStats } from "../composables/useRepoStats";
+import { useRepoStats } from "../composables/useRepoStats.ts";
 
 const { stats: repoStats, fetchRepoStats } = useRepoStats();
 
